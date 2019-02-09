cfbe7866bcf79021b4dbff5e44a2c1fa4449520c
Claire Oberdorf <claire.oberdorf1@marist.edu>
2019-02-08 22:22:47 -0500

    lab1

---
+++ labs/1/problem2.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class problem2 {
+
+	public static void main(String[] args) {
+		//Calculating PI values
+		double pi1 = 4 * (1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));
+		double pi2 = 4 * (1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13));
+		
+		//Display output
+		System.out.println("\n PI value is :"+ pi1);
+		System.out.println("\n PI value is :"+ pi2);
+	}
+
+}
