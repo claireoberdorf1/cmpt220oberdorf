235ef40402320acb7df81f5afaa84c12af1601b9
claireoberdorf1 <47015447+claireoberdorf1@users.noreply.github.com>
2019-03-29 23:02:12 -0400

    lab 6 java files

---
+++ labs/6/TestBond.java
@@ -0,0 +1,11 @@
+package lab6;
+
+public class TestBond{
+  public static void main(String[] args) {
+    Bond b = new Bond(500, 100, 0.05, 10000);
+      System.out.printf("\n Price: $%.2f \n", b.getPrice());
+    
+}
+}
+
+
